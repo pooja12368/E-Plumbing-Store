a3c278cda2d46e4ba9c2de64fb051ff6860b6d42
pooja12368 <147509852+pooja12368@users.noreply.github.com>
2024-04-06 14:24:17 +0530

    Delete src directory

--- src/main/java/com/eplumbing/daos/PaymentDao.java
+++
@@ -1,9 +0,0 @@
-package com.eplumbing.daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eplumbing.entities.Payment;
-
-public interface PaymentDao extends JpaRepository<Payment, Integer> {
-
-}
--- src/main/java/com/eplumbing/services/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.eplumbing.services;
-
-import com.eplumbing.entities.Payment;
-
-public interface PaymentService {
-	Payment savePayment(Payment payment);
-	Payment findPaymentById(int id);
-}
